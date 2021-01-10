547153a5373ef5f16f4892e49ceabc94ef1fca92
Дима Муженко <muzhnat@ukr.net>
2021-01-10 18:42:28 +0200

    more-tests

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -8,22 +8,9 @@ using IIG.PasswordHashingUtils;
 
 namespace Test
 {
-    public class UnitTest1
+    public class PasswordHasherTest
     {
-
-        private const string Login = @"sa";
-        private const string Password = @"1234";
-
-        private const string Server = @"DESKTOP-BUEC766";
-        private const string Database = @"IIG.CoSWE.FlagpoleDB";
-
-        private const bool IsTrusted = true;
-
-        private const int ConnectionTimeout = 15;
-
         private string path = BaseFileWorker.MkDir("FileWorker");
-
-        FlagpoleDatabaseUtils flagpoleDB = new FlagpoleDatabaseUtils(Server, Database, IsTrusted, Login, Password, ConnectionTimeout);
 
         [Fact]
         public void TestGetHash()
@@ -31,6 +18,62 @@ namespace Test
             string password = PasswordHasher.GetHash("password", "password");
             BaseFileWorker.Write(password, path + "\\" + "test.txt");
             Assert.Equal(password, BaseFileWorker.ReadAll(path + "\\" + "test.txt"));
+        }
+
+        [Fact]
+        public void TestGetHashEmptyString()
+        {
+            string password = PasswordHasher.GetHash(string.Empty, string.Empty);
+            BaseFileWorker.Write(string.Empty, path + "\\" + "test.txt");
+            string password2 = PasswordHasher.GetHash(BaseFileWorker.ReadAll(path + "\\" + "test.txt"));
+            Assert.Equal(password, password2);
+        }
+
+        [Fact]
+        public void TestGetHashSymbols()
+        {
+            string symbols = "!@#$%^&*()_+-=,./;'[]|}{:";
+            string password = PasswordHasher.GetHash(symbols, symbols);
+            BaseFileWorker.Write(symbols, path + "\\" + "test.txt");
+            string password2 = PasswordHasher.GetHash(BaseFileWorker.ReadAll(path + "\\" + "test.txt"));
+            Assert.Equal(password, password2);
+        }
+
+        [Fact]
+        public void TestGetHashFromFile()
+        {
+            BaseFileWorker.Write("password", path + "\\" + "password.txt");
+            BaseFileWorker.Write("salt", path + "\\" + "salt.txt");
+            string password = PasswordHasher.GetHash("password", "salt");
+            string password2 = PasswordHasher.GetHash(
+                BaseFileWorker.ReadAll(path + "\\" + "password.txt"), 
+                BaseFileWorker.ReadAll(path + "\\" + "salt.txt")
+            );
+            Assert.Equal(password, password2);
+        }
+
+        [Fact]
+        public void TestGetHashEmptyFilename()
+        {
+            string password = PasswordHasher.GetHash("password", "password");
+            BaseFileWorker.Write(password, path + "\\" + string.Empty);
+            Assert.NotEqual(password, BaseFileWorker.ReadAll(path + "\\" + string.Empty));
+        }
+
+        [Fact]
+        public void TestGetHashDifferentExtension()
+        {
+            string password = PasswordHasher.GetHash("password", "password");
+            BaseFileWorker.Write(password, path + "\\" + "test.js");
+            Assert.Equal(password, BaseFileWorker.ReadAll(path + "\\" + "test.js"));
+        }
+
+        [Fact]
+        public void TestGetHashExtensionsOnly()
+        {
+            string password = PasswordHasher.GetHash("password", "password");
+            BaseFileWorker.Write(password, path + "\\" + ".txt");
+            Assert.Equal(password, BaseFileWorker.ReadAll(path + "\\" + ".txt"));
         }
 
         [Fact]
@@ -53,6 +96,21 @@ namespace Test
             BaseFileWorker.Write(password2, path + "\\" + "test2.txt");
             Assert.NotEqual(BaseFileWorker.ReadAll(path + "\\" + "test1.txt"), BaseFileWorker.ReadAll(path + "\\" + "test2.txt"));
         }
+    }
+
+    public class BinaryFlagTest
+    {
+        private const string Login = @"sa";
+        private const string Password = @"1234";
+
+        private const string Server = @"DESKTOP-BUEC766";
+        private const string Database = @"IIG.CoSWE.FlagpoleDB";
+
+        private const bool IsTrusted = true;
+
+        private const int ConnectionTimeout = 15;
+
+        FlagpoleDatabaseUtils flagpoleDB = new FlagpoleDatabaseUtils(Server, Database, IsTrusted, Login, Password, ConnectionTimeout);
 
         [Fact]
         public void TestAddFlagTrue()
@@ -70,6 +128,34 @@ namespace Test
             string flagView = bFlag.ToString();
             bool flagValue = bFlag.GetFlag();
             Assert.True(flagpoleDB.AddFlag(flagView, flagValue));
+        }
+
+        [Fact]
+        public void TestAddFlagMixed()
+        {
+            MultipleBinaryFlag bFlag = new MultipleBinaryFlag(10, false);
+            for (ulong i = 0; i < 10; i += 2) bFlag.SetFlag(i);
+            string flagView = bFlag.ToString();
+            bool flagValue = bFlag.GetFlag();
+            Assert.True(flagpoleDB.AddFlag(flagView, flagValue));
+        }
+
+        [Fact]
+        public void TestAddFlagManualInputFalse()
+        {
+            Assert.True(flagpoleDB.AddFlag("TTFFTFTF", false));
+        }
+
+        [Fact]
+        public void TestAddFlagManualInputTrue()
+        {
+            Assert.True(flagpoleDB.AddFlag("TTTTTT", true));
+        }
+
+        [Fact]
+        public void TestAddFlagManualInputBroken()
+        {
+            Assert.False(flagpoleDB.AddFlag("broken input", true));
         }
 
         [Fact]
@@ -103,10 +189,26 @@ namespace Test
         }
 
         [Fact]
-        public void TestGetFlag()
+        public void TestGetFlagMixed()
         {
             MultipleBinaryFlag bFlag = new MultipleBinaryFlag(10);
             for (ulong i = 0; i < 10; i += 2) bFlag.ResetFlag(i);
+            for (ulong i = 0; i < 10; i += 3) bFlag.SetFlag(i);
+            string flagView = bFlag.ToString();
+            bool flagValue = bFlag.GetFlag();
+            flagpoleDB.AddFlag(flagView, flagValue);
+            string flagViewCheck;
+            bool? flagValueCheck;
+            int flagId = (int)flagpoleDB.GetIntBySql($"SELECT MultipleBinaryFlagID FROM dbo.MultipleBinaryFlags WHERE MultipleBinaryFlagView = '{flagView}'");
+            flagpoleDB.GetFlag(flagId, out flagViewCheck, out flagValueCheck);
+            Assert.Equal(flagValue, flagValueCheck);
+            Assert.Equal(flagView, flagViewCheck);
+        }
+
+        [Fact]
+        public void TestGetFlagLong()
+        {
+            MultipleBinaryFlag bFlag = new MultipleBinaryFlag(10000);
             string flagView = bFlag.ToString();
             bool flagValue = bFlag.GetFlag();
             flagpoleDB.AddFlag(flagView, flagValue);
